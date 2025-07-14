b2fba514d580950b5cbe8587bf9da78ef20a56cf
Shadab Ansari <shadabansari8319@gmail.com>
2025-07-14 16:19:58 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -1,53 +1,24 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import {app} from "./app.js"
 dotenv.config({path:'./.env'});
 
-connectDB();
-
-
-
-
-
-// import dotenv from "dotenv";
-// dotenv.config({ path: "./.env" });
-
-// console.log("✅ ENV PORT:", process.env.PORT);
-// console.log("✅ ENV MONGODB_URI:", process.env.MONGODB_URI);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express";
-
-// const app = express();
-
-
-// ;(async ()=>{
-//     try {
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//        app.on("error",(e)=>{
-//         console.log("ERROR: ",e);
-//         throw error;
-//        });
-
-//        app.listen(process.env.PORT,()=>{
-//         console.log(`Server is start at port ${process.env.PORT}`);
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is rnning at PORT: ",process.env.PORT);
         
-//        })
-//     } catch (error) {
-//         console.error("Error: ",error);
-//         throw error
-        
-//     }
-// })()
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb Connection Failed: ",err);
+    
+})
+
+
+
+
+
+
+
