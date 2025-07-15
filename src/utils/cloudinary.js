407dc647eed9e3e7fe6ea43e5b474b54377b9da4
Shadab Ansari <shadabansari8319@gmail.com>
2025-07-16 00:51:00 +0530

    add multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+    
+const uploadOnCloudinary = async (filePath)=> {
+
+    try {
+        if(!filePath) return null;
+
+     const response=  await cloudinary.uploader.upload(filePath,{resource_type:"auto"});
+        console.log("File Successfully uploaded on Cloudinary",response.url);
+
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(filePath) //Remove the locally save temp. file if the upload operation failed
+        return null;
+        
+    }
+}
+
+module.exports = {
+    uploadOnCloudinary
+}
